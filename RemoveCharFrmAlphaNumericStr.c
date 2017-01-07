6f44197cc2c4d4efaa6890cebc328b7ffe917737
DHANUSHXENO <dhanushxenocrate@gmail.com>
2017-01-07 18:53:00 +0530

    Add files via upload

--- RemoveCharFrmAlphaNumericStr.c
+++ RemoveCharFrmAlphaNumericStr.c
@@ -4,7 +4,7 @@ main()
     int i;char ch[20];
     printf("Enter a string::");
     gets(ch);
-    for(i=0;i<strlen(ch);i++)
+    for(i=0;ch[i]!='\0';i++)
     {
         if(ch[i]>='0' && ch[i]<='9')
             printf("%c",ch[i]);
