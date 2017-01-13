af6c702013aace0864a4188b5b1149f34b8b6de7
DHANUSHXENO <dhanushxenocrate@gmail.com>
2017-01-13 17:42:40 +0530

    Add files via upload

--- SumOfPowerValues.c
+++ SumOfPowerValues.c
@@ -1,13 +1,38 @@
 #include<stdio.h>
 main()
 {
-    int num,sum=0,a;
+    int num=0,sum=0,a=0,x,count=0,i,temp;
     printf("Enter a number");
-    scanf("%d",&num);
+    scanf("%d",&x);
+    num=x;
+    while(x!=0)
+    {
+        x%10;
+        ++count;
+        x=x/10;
+    }
     while(num!=0)
     {
         a=num%10;
-        sum=sum+(a*a*a*a);
+        if(count==2)
+            a=a*a;
+        else if(count==3)
+            a=a*a*a;
+        else if(count==4)
+            a=a*a*a*a;
+        else if(count==5)
+            a=a*a*a*a*a;
+        else if(count==6)
+            a=a*a*a*a*a*a;
+        else if(count==7)
+            a=a*a*a*a*a*a*a;
+        else if(count==8)
+            a=a*a*a*a*a*a*a*a;
+        else if(count==9)
+            a=a*a*a*a*a*a*a*a;
+        else
+            a=a;
+        sum=sum+a;
         num=num/10;
     }
     printf("Answer = %d",sum);
